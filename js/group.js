334e427dcd04f3a37ca2fdb150aa05976ec4a4d0
Beau324 <Beau.cunningham324@gmail.com>
2020-10-16 12:28:23 -0500

    fix loan details and input

--- js/group.js
+++ js/group.js
@@ -1,19 +1,21 @@
 // JS dragon group added
 
 /* Wired up Input Variables */
-var inputLoanAmount = document.getElementById("amount").value;
-var inputInterestRate = document.getElementById("interest").value;
-var inputLoanTenure = document.getElementById("tenure").value;
+
 
 // not working yet
-function calcMonthlyPayment () {
+function calcMonthlyPayment() {
+    var inputLoanAmount = parseInt(document.getElementById("amount").value);
+    var inputInterestRate = parseFloat(document.getElementById("interest").value);
+    var inputLoanTenure = parseInt(document.getElementById("tenure").value);
+
     let monthly = inputInterestRate / 1200;
     let annual = inputInterestRate / 100;
     let emi = inputLoanAmount * monthly * (Math.pow(1 + monthly, inputLoanTenure)) / (Math.pow(1 + monthly, inputLoanTenure) - 1);
     let tInterest = inputLoanAmount * annual;
-    let tPayment = tInterest + inputLoanAmount;
-    document.getElementById("emi").innerHTML = `<h5>${emi}</h5>`;
+    let tPayment = inputLoanAmount + tInterest;
+    document.getElementById("emi").innerHTML = `<h5>${emi.toFixed(0)}</h5>`;
     document.getElementById("tInterest").innerHTML = `<h5>${tInterest}</h5>`;
     document.getElementById("tPayment").innerHTML = `<h5>${tPayment}</h5>`;
-    donutData.setData([{ "Principal": inputLoanAmount.value},{ "Interest": "2006"}]);
+//    donutData.setData([{ "Principal": inputLoanAmount.value},{ "Interest": "2006"}]);
 } 
