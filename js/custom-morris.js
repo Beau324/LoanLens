3a56c98568c7b88e04f85ff3064614c832c91408
Beau324 <Beau.cunningham324@gmail.com>
2020-10-16 07:16:21 -0500

    just pulling

--- js/custom-morris.js
+++ js/custom-morris.js
@@ -1,22 +1,22 @@
 var $donutData = [];
 (function ($) {
-  // "use strict"; // Start of use strict
- 
-  // morris donut charts
-  if($("#donutMorris").length == 1){
-    $donutData = [
-    { label: "Principal", value: 200000 },
-    { label: "Interest", value: 50000 },
-  ];
-  Morris.Donut({
-    element: 'donutMorris',
-    data: $donutData,
-    barSize: 0.1,
-    labelColor: '#3e5569',
-    resize: true, //defaulted to true
-    colors: ['#84BD5A', '#322153']
-  });
-  } 
-  
+    // "use strict"; // Start of use strict
+
+    // morris donut charts
+    if ($("#donutMorris").length == 1) {
+        $donutData = [
+            { label: "Principal", value: 200000 },
+            { label: "Interest", value: 50000 },
+        ];
+        Morris.Donut({
+            element: 'donutMorris',
+            data: $donutData,
+            barSize: 0.1,
+            labelColor: '#3e5569',
+            resize: true, //defaulted to true
+            colors: ['#84BD5A', '#322153']
+        });
+    }
+
 })(jQuery);
 
