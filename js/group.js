e0ddc4ec67fb8b710f758128a2d39dab9f084d01
Julio Segarra <42501108+juliosegarra@users.noreply.github.com>
2020-10-16 12:39:29 -0400

    added button type to button so no refresh, working on donut

--- js/group.js
+++ js/group.js
@@ -5,7 +5,8 @@ const inputLoanAmount = document.getElementById("amount");
 const inputInterestRate = document.getElementById("interest");
 const inputLoanTenure = document.getElementById("tenure");
 
-
+// not working yet
 function calcMonthlyPayment () {
-    alert('Hi');
+    
+    donutData.setData([{ "Principal": inputLoanAmount.value},{ "Interest": "2006"}]);
 } 
