e682453aea1022a8a82abf824b269df9c738ba8f
Julio Segarra <42501108+juliosegarra@users.noreply.github.com>
2020-10-16 10:43:52 -0400

    added JS variables for our 3 inputs

--- js/group.js
+++ js/group.js
@@ -1,2 +1,6 @@
 // JS dragon group added
 
+const inputLoanAmount = document.getElementById("amount");
+const inputInterestRate = document.getElementById("interest");
+const inputLoanTenure = document.getElementById("tenure");
+
