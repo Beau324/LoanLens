b983f98e7c6e11492945c251ac0b770d04930cf5
Beau324 <Beau.cunningham324@gmail.com>
2020-10-16 18:12:34 -0500

    table maker

--- js/group.js
+++ js/group.js
@@ -25,22 +25,17 @@ function calcMonthlyPayment() {
         balance = inputLoanAmount.value;
 
         var arr = new Array();
-    for (i = 0; i < inputLoanTenure.value; i++) {
+    for (i = 0; i < inputLoanTenure; i++) {
         totalInterest += (balance * (monthly));
         arr[i] = new Array();
         arr[i][0] = i + 1;
         arr[i][1] = emi;
-        arr[i][2] = emi - (balance * (monthly));
+        arr[i][2] = parseInt(emi - (balance * (monthly))).value;
         arr[i][3] = balance * (monthly);
         arr[i][4] = totalInterest;
         arr[i][5] = (balance - emi);
         balance -= emi;
-        console.log(arr[i][0]);
-        console.log(arr[i][1]);
-        console.log(arr[i][2]);
-        console.log(arr[i][3]);
-        console.log(arr[i][4]);
-        console.log(arr[i][5]);
+        
     }
    
 
@@ -52,7 +47,13 @@ function calcMonthlyPayment() {
         for (let j = 0; tableRef.rows.length > 0; j++) {
             tableRef.deleteRow(-1);
         }
-        for (let k = 0; k < inputLoanTenure.value; k++) {
+    for (let k = 0; k < inputLoanTenure; k++) {
+        console.log(arr[k][0]);
+        console.log(arr[k][1]);
+        console.log(arr[k][2]);
+        console.log(arr[k][3]);
+        console.log(arr[k][4]);
+        console.log(arr[k][5]);
             let newRow = tableRef.insertRow();
             var myHTML = `<td> + ${arr[k][0]} + </td><td> + ${arr[k][1]} + </td><td> + ${arr[k][2]} + </td><td> + ${arr[k][3]} + </td><td> + ${arr[k][4]} + </td><td> + ${arr[k][5]} + </td>`;
             newRow.innerHTML = myHTML;
