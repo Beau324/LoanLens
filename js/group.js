71fc492a8d2cf54c8aff8dc6f8107028d147c293
Beau324 <Beau.cunningham324@gmail.com>
2020-10-18 22:39:50 -0500

    curremcy formatter, code page fixed nav position, table header fixed position

--- js/group.js
+++ js/group.js
@@ -8,7 +8,16 @@ document.getElementById("calc").addEventListener(`click`, function () {
     document.getElementById("tableHidden").style.display = "block";
 });
 
-// not working yet
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2
+})
+
+//formatter.format(1000) // "$1,000.00"
+//formatter.format(10) // "$10.00"
+//formatter.format(123233000) // "$123,233,000.00"
+
 function calcMonthlyPayment() {
     var inputLoanAmount = parseFloat(document.getElementById("amount").value);
     var inputInterestRate = parseFloat(document.getElementById("interest").value);
@@ -19,9 +28,9 @@ function calcMonthlyPayment() {
     const emi = (inputLoanAmount * monthly) / (1.0 - Math.pow(1.0 + monthly, parseInt(inputLoanTenure * -1)));
     let tInterest = emi * inputLoanTenure - inputLoanAmount;
     let tPayment = inputLoanAmount + tInterest;
-    document.getElementById("emi").innerHTML = `<h5>${emi.toFixed(2)}</h5>`;
-    document.getElementById("tInterest").innerHTML = `<h5>${tInterest.toFixed(2)}</h5>`;
-    document.getElementById("tPayment").innerHTML = `<h5>${tPayment.toFixed(2)}</h5>`;
+    document.getElementById("emi").innerHTML = `<h5>${formatter.format(emi)}</h5>`;
+    document.getElementById("tInterest").innerHTML = `<h5>${formatter.format(tInterest)}</h5>`;
+    document.getElementById("tPayment").innerHTML = `<h5>${formatter.format(tPayment)}</h5>`;
 
     // Donut update
     $donutData = [{ label: "Principal", value: inputLoanAmount.toFixed(0) }, { label: "Interest", value: tInterest.toFixed(0) }];
@@ -71,7 +80,7 @@ function calcMonthlyPayment() {
         }
     for (let k = 0; k < inputLoanTenure; k++) {
             let newRow = tableRef.insertRow();
-        var myHTML = `<td>${(arr[k][0])}</td><td>${(arr[k][1])}</td><td>${(arr[k][2])}</td><td>${(arr[k][3])}</td><td>${(arr[k][4])}</td><td>${(arr[k][5])}</td>`;
+        var myHTML = `<td>${(arr[k][0])}</td><td>${(formatter.format(arr[k][1]))}</td><td>${(formatter.format(arr[k][2]))}</td><td>${(formatter.format(arr[k][3]))}</td><td>${(formatter.format(arr[k][4]))}</td><td>${(formatter.format(arr[k][5]))}</td>`;
             newRow.innerHTML = myHTML;
         }
  
